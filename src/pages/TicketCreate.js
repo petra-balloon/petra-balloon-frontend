e7c679afce969478e30b6f555fd658ce590304c5
Qasim Ali <54708575+qasimlucky@users.noreply.github.com>
2023-07-04 19:53:31 +0500

    qasim

--- src/pages/TicketCreate.js
+++ src/pages/TicketCreate.js
@@ -1,9 +1,32 @@
 import React from "react";
+import QRCode from "react-qr-code";
 
+const ticketHTML = `
+Petra Balloon
+Name  : Qasim
+`;
 const TicketCreate = () => {
   return (
     <>
-      <div className="ticket-outer-class">this is ticket</div>
+      <div className="ticket-outer-class">
+        <div style={{ display: "flex" }}>
+          <div className="img-div">
+            <img style={{ height: "206px" }} src="assets/img/team/4.jpg"></img>
+          </div>
+          <div className="outer-ticket-content-div">
+            <div className="logo-div-tiket"></div>
+            <div className="ticket-div-info">
+              <div className="qr-code-div">
+                <QRCode size={60} value={ticketHTML} />
+              </div>
+              <div className="inside-content-div">
+                02 july 2023 - Ticket 28642 <br></br> <b>Adult +12 years</b>
+              </div>
+            </div>
+            <div className="company-info-div"></div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
