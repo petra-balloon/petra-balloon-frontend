0bcb445ee9c318cb0cc9b478dd87b99fc06320de
Qasim Ali <54708575+qasimlucky@users.noreply.github.com>
2023-07-05 21:13:55 +0500

    qasim

--- src/components/TeamAreaGroup.js
+++ src/components/TeamAreaGroup.js
@@ -1,18 +1,42 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const TeamAreaGroup = () => {
+const [instaAllFeed,setInstaAllFeed] = useState()
+  useEffect(() => {
+    getInstagramFeed();
+  }, []);
+
+
+  const getInstagramFeed = async (email, password) => {
+    //setIsLoading(true);
+    const YOUR_ACCESS_TOKEN = 'IGQVJVQUo1RWFfYURVcU1KbzZAvUDNKbEk3bjZAjWWR3MV93V3paNTlfWVR1bmNSNXVzLWRIS0xIUXBoemRvZAlJLS1pPekFqbFpIT0dPejRRVXNrYTVLMXI0TGlEeU1xX3lsYk5SVHhlVWVFSUF3SHVlRwZDZD'
+    await axios
+      .get(`https://graph.instagram.com/me/media?fields=id,media_url,caption&access_token=${YOUR_ACCESS_TOKEN}`, {})
+      .then(async (response) => {
+        console.log("this is response",response.data.data[0].media_url)
+        console.log(response.data);
+        setInstaAllFeed(response.data.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   return (
     <>
       {/* ================= TeamAreaGroup Start =================*/}
       <div className="team-area bg-relative pd-top-120 pd-bottom-90">
         <div className="container">
           <div className="row">
-            <div className="col-6">
+          {instaAllFeed &&
+              instaAllFeed.map((Details) => (
+            <div className="col-4">
               <div className="single-team-inner text-center">
                 <div className="thumb">
-                  <img src="assets/img/team/1.jpg" alt="img" />
+                  <img style={{width:"98%",height:"300px"}} src={Details.media_url} alt="img" />
                   {/* <ul className="team-social-inner">
                     <li>
                       <a href="#">
@@ -33,69 +57,30 @@ const TeamAreaGroup = () => {
                 </div>
               </div>
             </div>
-            <div className="col-6">
+            ))}
+           {/*  <div className="col-6">
               <div className="single-team-inner text-center">
                 <div className="thumb">
                   <img src="assets/img/team/2.jpg" alt="img" />
-                  {/* <ul className="team-social-inner">
-                    <li>
-                      <a href="#">
-                        <FaFacebookF />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <FaTwitter />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <FaInstagram />
-                      </a>
-                    </li>
-                  </ul> */}
+
                 </div>
               </div>
-            </div>
-            <div className="col-6">
+            </div> */}
+            {/* <div className="col-6">
               <div className="single-team-inner text-center">
                 <div className="thumb">
                   <img src="assets/img/team/3.jpg" alt="img" />
-                  {/* <ul className="team-social-inner">
-                    <li>
-                      <a href="#">
-                        <FaFacebookF />
-                      </a>
-                    </li>
-                  
-                  </ul> */}
                 </div>
               </div>
-            </div>
-            <div className="col-6">
+            </div> */}
+            {/* <div className="col-6">
               <div className="single-team-inner text-center">
                 <div className="thumb">
                   <img src="assets/img/team/4.jpg" alt="img" />
-                  {/* <ul className="team-social-inner">
-                    <li>
-                      <a href="#">
-                        <FaFacebookF />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <FaTwitter />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <FaInstagram />
-                      </a>
-                    </li>
-                  </ul> */}
+                
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
