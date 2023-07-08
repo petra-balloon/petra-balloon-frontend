8867f9697e1ab41aca7662ef3e1c9b6bfbb302bd
Qasim Ali <54708575+qasimlucky@users.noreply.github.com>
2023-07-08 14:00:03 +0500

    qasim

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 //export const API_URL = "https://petra-balloon.herokuapp.com/api/";
-//export const image_url = "http://localhost:5000/static/"
-//export const API_URL = "http://localhost:5000/api/";
-export const API_URL = "http://139.59.45.111/api/"
+export const image_url = "http://localhost:5000/static/"
+export const API_URL = "http://localhost:5000/api/";
+//export const API_URL = "http://139.59.45.111/api/"
 
- export const image_url = "http://139.59.45.111/static/"
+ //export const image_url = "http://139.59.45.111/static/"
