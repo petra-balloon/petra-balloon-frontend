f812d7ea0fd6cfec05465043278b7cffd9db2393
Qasim Ali <54708575+qasimlucky@users.noreply.github.com>
2023-07-26 15:51:31 +0500

    final push

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 //export const API_URL = "https://petra-balloon.herokuapp.com/api/";
 //export const image_url = "http://localhost:5000/static/"
-export const API_URL = "http://localhost:5000/api/";
+//export const API_URL = "http://localhost:5000/api/";
 //export const API_URL = "http://139.59.45.111/api/"
 
  //export const image_url = "http://139.59.45.111/static/"
@@ -8,4 +8,7 @@ export const API_URL = "http://localhost:5000/api/";
 
  //rendor
 //export const API_URL = "https://petra-balloon-backend.onrender.com/api/";
-export const image_url = "https://petra-balloon-backend.onrender.com/static/";
+//export const image_url = "https://petra-balloon-backend.onrender.com/static/";
+
+export const API_URL = "https://api.petraballoon.com/api/"
+export const image_url = "https://api.petraballoon.com/static/";
